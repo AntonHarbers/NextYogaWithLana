73f30282e70408707f683d8ba74c08776be56934
Cosmicode <anton.harbers23@gmail.com>
2024-09-03 16:27:09 +0200

    PATCH: updated website title to Lana Harbers Educator & Coach

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='flex flex-col h-screen scrollbar-thin scrollbar-thumb-secondary scrollbar-track-primary overflow-x-hidden'>
       <Head>
-        <title>Lana Harbers Yoga</title>
+        <title>Lana Harbers Educator & Coach</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
